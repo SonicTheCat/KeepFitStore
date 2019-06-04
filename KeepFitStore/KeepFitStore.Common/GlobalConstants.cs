3605ef04a4364b2418b29f837ef3658ea179cb57
SonicTheCat <paveliliev90@gmail.com>
2019-06-05 02:59:43 +0300

    Common namespace introduced. Added class GlobalConstants

---
+++ KeepFitStore/KeepFitStore.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace KeepFitStore.Common
+{
+    public static class GlobalConstants
+    {
+        public const string AdministratorRoleName = "Aministrator"; 
+
+        public const string UserRoleName = "User"; 
+    }
+}
