5d718f07d92f85b0c3cb41cf94e0a9365a428c84
SonicTheCat <paveliliev90@gmail.com>
2019-07-09 06:28:28 +0300

    CreateReviewInput model class introduced! Review Controller added, but not implemented!

---
+++ KeepFitStore/KeepFitStore.Models/InputModels/Reviews/CreateReviewInputModel.cs
@@ -0,0 +1,15 @@
+﻿namespace KeepFitStore.Models.InputModels.Reviews
+{
+    public class CreateReviewInputModel
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int GivenRating { get; set; }
+
+        public string KeepFitUserId { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
---
+++ KeepFitStore/KeepFitStore.WEB/Controllers/ReviewController.cs
@@ -0,0 +1,17 @@
+﻿using KeepFitStore.Models.InputModels.Reviews;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KeepFitStore.WEB.Controllers
+{
+    public class ReviewController : BaseController
+    {
+        public IActionResult Create(CreateReviewInputModel model)
+        {
+            throw new NotImplementedException(); 
+        }
+    }
+}
