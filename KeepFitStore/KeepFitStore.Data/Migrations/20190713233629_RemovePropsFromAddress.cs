61d38ab0e5fbd7af323e79f4c09b500ede1df726
SonicTheCat <paveliliev90@gmail.com>
2019-07-14 02:39:42 +0300

    Two properties removed from Address Entity

---
+++ KeepFitStore/KeepFitStore.Data/Migrations/20190713233629_RemovePropsFromAddress.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KeepFitStore.Data.Migrations
+{
+    public partial class RemovePropsFromAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BuildingNumebr",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "RegionName",
+                table: "Addresses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StreetNumber",
+                table: "Addresses",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "StreetNumber",
+                table: "Addresses",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<int>(
+                name: "BuildingNumebr",
+                table: "Addresses",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RegionName",
+                table: "Addresses",
+                nullable: true);
+        }
+    }
+}
--- KeepFitStore/KeepFitStore.Domain/Address.cs
+++ KeepFitStore/KeepFitStore.Domain/Address.cs
@@ -11,13 +11,9 @@
 
         public int Id { get; set; }
 
-        public int? StreetNumber { get; set; }
+        public int StreetNumber { get; set; }
 
         public string StreetName { get; set; }
-
-        public int? BuildingNumebr { get; set; }
-
-        public string RegionName { get; set; }
 
         public int CityId { get; set; }
         public City City { get; set; }
