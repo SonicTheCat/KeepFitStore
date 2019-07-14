f24871e736df6f38b2ef750e795acc7dd7201d52
SonicTheCat <paveliliev90@gmail.com>
2019-07-14 04:43:31 +0300

    CreateAddressInputModel and CreateAddressViewModel introduced!

---
+++ KeepFitStore/KeepFitStore.Models/InputModels/Address/CreateAddressInputModel.cs
@@ -0,0 +1,29 @@
+﻿namespace KeepFitStore.Models.InputModels.Address
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using KeepFitStore.Models.Common;
+
+    public class CreateAddressInputModel
+    {
+        [Required]
+        [StringLength(ModelsConstants.CityNameMaxLength,
+            MinimumLength = ModelsConstants.CityNameMinLength,
+            ErrorMessage = ModelsConstants.StringErrorMessage)]
+        public string CityName { get; set; }
+
+        [Required]
+        public string PostCode { get; set; }
+
+
+        [Required]
+        [StringLength(ModelsConstants.StreetNameMaxLength,
+            MinimumLength = ModelsConstants.StreetNameMinLength,
+            ErrorMessage = ModelsConstants.StringErrorMessage)]
+        public string StreetName { get; set; }
+
+        [Required]
+        [Range(ModelsConstants.StreetNumberMinNumber, ModelsConstants.StreetNumberMaxNumber)]
+        public int StreetNumber { get; set; }
+    }
+}
---
+++ KeepFitStore/KeepFitStore.Models/ViewModels/Address/CreateAddressViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace KeepFitStore.Models.ViewModels.Address
+{
+    public class CreateAddressViewModel
+    {
+        public int Id { get; set; }
+
+        public int StreetNumber { get; set; }
+
+        public string StreetName { get; set; }
+
+        public int CityId { get; set; }
+
+        public string CityName { get; set; }
+
+        public string CityPostCode { get; set; }
+    }
+}
