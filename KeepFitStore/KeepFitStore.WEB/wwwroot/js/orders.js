8df12a7d9a6d31889f4b5258ab3aa97481b6a635
SonicTheCat <paveliliev90@gmail.com>
2019-07-14 05:09:42 +0300

    Javascript functions separated, so two more js files were created!

---
+++ KeepFitStore/KeepFitStore.WEB/wwwroot/js/orders.js
@@ -0,0 +1,40 @@
+﻿const addressForm = $("#address-form");
+const addressDiv = $("#address-div");
+const billingDiv = $("#billing-div")
+
+function addAddress() {
+    addressForm.submit(function (evt) {
+        evt.preventDefault();
+
+        let isFormValid = addressForm.valid();
+        if (!isFormValid) {
+            return;
+        }
+
+        let data = {
+            CityName: $("#DeliveryAddress_City_Name").val(),
+            PostCode: $("#DeliveryAddress_City_PostCode").val(),
+            StreetName: $("#DeliveryAddress_StreetName").val(),
+            StreetNumber: $("#DeliveryAddress_StreetNumber").val()
+        };
+
+        var antiForgery = $('input[name="__RequestVerificationToken"]').val();
+
+        $.ajax({
+            type: "POST",
+            url: "/api/AddressApi/Create",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(data),
+            beforeSend: function (request) {
+                request.setRequestHeader("RequestVerificationToken", antiForgery);
+            },
+            success: function (data) {
+                addressDiv.toggle("slow");
+                billingDiv.toggle("slow");
+            },
+            error: function (er) {
+                console.log(er)
+            }
+        });
+    });
+}
---
+++ KeepFitStore/KeepFitStore.WEB/wwwroot/js/reviews.js
@@ -0,0 +1,62 @@
+﻿function submitReviewForm() {
+    $("form").on("submit", function (evt) {
+        evt.preventDefault();
+        var title = $("#title").val();
+        var rating = $("#rating").val();
+        var content = $("#content").val();
+        var productId = $("textarea").attr("productId");
+
+        let inputModel = {
+            Title: title,
+            GivenRating: rating,
+            Content: content,
+            ProductId: productId
+        };
+
+        var antiForgery = $('input[name="__RequestVerificationToken"]').val();
+
+        $.ajax({
+            type: "POST",
+            url: "/api/ReviewsApi/Create",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(inputModel),
+            beforeSend: function (request) {
+                request.setRequestHeader("RequestVerificationToken", antiForgery);
+            },
+            success: function (data) {
+                location.reload();
+            },
+            error: function (err) {
+                let errorDiv = $("#error-message");
+                let errorAsJson = err.responseJSON.errors;
+                errorDiv
+                    .empty()
+                    .show()
+                    .append("<p>All Lines are required!</p>");
+
+                if (err.status === 400) {
+                    if (errorAsJson.GivenRating !== undefined) {
+                        errorDiv.append("<p>Rating value must be between 1 and 5</p>");
+
+                    } else {
+                        for (let e in errorAsJson) {
+                            errorDiv.append(`<p>${errorAsJson[e][0]}</p>`);
+                        }
+                    }
+                }
+            }
+        });
+    });
+}
+
+function goToReviewsForCurrentPrdocut() {
+    $("#rating-stars").on("click", function () {
+        $("#nav-reviews-tab").click();
+
+        setTimeout(function () {
+            $('html, body').animate({
+                scrollTop: ($('#nav-reviews').offset().top)
+            }, 800)
+        }, 200);
+    });
+}
