b6ac5059c60cfae7b96f9c80844c0f395715beb4
SonicTheCat <paveliliev90@gmail.com>
2019-07-14 04:47:02 +0300

    AddressApiController added

---
+++ KeepFitStore/KeepFitStore.WEB/Controllers/AddressApiController.cs
@@ -0,0 +1,29 @@
+﻿namespace KeepFitStore.WEB.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+   
+    using KeepFitStore.Models.InputModels.Address;
+    using KeepFitStore.Services.Contracts;
+    using KeepFitStore.Models.ViewModels.Address;
+
+    public class AddressApiController : ApiController
+    {
+        private readonly IAddressService addressService;
+
+        public AddressApiController(IAddressService addressService)
+        {
+            this.addressService = addressService;
+        }
+
+        [HttpPost]
+        [ActionName(nameof(Create))]
+        public async Task<ActionResult<CreateAddressViewModel>> Create(CreateAddressInputModel model)
+        {
+            var createdObj = await this.addressService.AddAddressToUserAsync(model, this.User);
+
+            return createdObj; 
+        }
+    }
+}
---
+++ KeepFitStore/KeepFitStore.WEB/Controllers/ApiController.cs
@@ -0,0 +1,10 @@
+﻿namespace KeepFitStore.WEB.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+    }
+}
