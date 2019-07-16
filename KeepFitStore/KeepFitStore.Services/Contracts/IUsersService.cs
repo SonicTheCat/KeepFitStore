1ae1529fba0aca90ab2a84990decfb272fe55f52
SonicTheCat <paveliliev90@gmail.com>
2019-07-16 22:03:20 +0300

    UserServices added

---
+++ KeepFitStore/KeepFitStore.Services/Contracts/IUsersService.cs
@@ -0,0 +1,13 @@
+﻿namespace KeepFitStore.Services.Contracts
+{
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using KeepFitStore.Models.InputModels.User;
+    using KeepFitStore.Models.ViewModels.User;
+
+    public interface IUsersService
+    {
+        Task<UpdateUserViewModel> UpdateUserOrderInfoAsync(ClaimsPrincipal claimsPrincipal, UpdateUserInputModel model); 
+    }
+}
---
+++ KeepFitStore/KeepFitStore.Services/UsersSerivce.cs
@@ -0,0 +1,40 @@
+﻿namespace KeepFitStore.Services
+{
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Identity;
+
+    using AutoMapper;
+
+    using KeepFitStore.Data;
+    using KeepFitStore.Domain;
+    using KeepFitStore.Models.InputModels.User;
+    using KeepFitStore.Models.ViewModels.User;
+    using KeepFitStore.Services.Contracts;
+    
+    public class UsersSerivce : IUsersService
+    {
+        private readonly UserManager<KeepFitUser> userManager;
+        private readonly KeepFitDbContext context;
+        private readonly IMapper mapper;
+
+        public UsersSerivce(UserManager<KeepFitUser> userManager, KeepFitDbContext context, IMapper mapper)
+        {
+            this.userManager = userManager;
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public async Task<UpdateUserViewModel> UpdateUserOrderInfoAsync(ClaimsPrincipal principal, UpdateUserInputModel model)
+        {
+            var user = await this.userManager.GetUserAsync(principal);
+            user.FullName = model.FullName;
+            user.PhoneNumber = model.PhoneNumber;
+            await this.context.SaveChangesAsync();
+
+            var viewModel = this.mapper.Map<UpdateUserViewModel>(user); 
+            return viewModel;
+        }
+    }
+}
