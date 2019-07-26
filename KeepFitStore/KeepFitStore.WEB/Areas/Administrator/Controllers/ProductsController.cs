8eb03da0168ae23132356d2a33cdb2fca1b13d5c
SonicTheCat <paveliliev90@gmail.com>
2019-07-26 15:58:50 +0300

    ProductsController added in Administrator Area. Create view and Index view implemented as well!

---
+++ KeepFitStore/KeepFitStore.WEB/Areas/Administrator/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿namespace KeepFitStore.WEB.Areas.Administrator.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using KeepFitStore.Services.Contracts;
+
+    public class ProductsController : AdministratorController
+    {
+        private readonly IProductsService productsSerive;
+
+        public ProductsController(IProductsService productsSerive)
+        {
+            this.productsSerive = productsSerive;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = await this.productsSerive.GetAllAsync();
+
+            return this.View(viewModel);
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+    }
+}
