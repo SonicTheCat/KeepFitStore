d6958667d0fc95600103cb21194a82fa8030b523
SonicTheCat <paveliliev90@gmail.com>
2019-07-30 14:14:53 +0300

    Service for payments introduced!

---
+++ KeepFitStore/KeepFitStore.Services/Contracts/IPaymentService.cs
@@ -0,0 +1,9 @@
+﻿namespace KeepFitStore.Services.Contracts
+{
+    using System.Threading.Tasks;
+
+    public interface IPaymentService 
+    {
+        Task<bool> MakePaymentAsync(string email, string token, decimal amount); 
+    }
+}
---
+++ KeepFitStore/KeepFitStore.Services/PaymentService.cs
@@ -0,0 +1,53 @@
+﻿namespace KeepFitStore.Services
+{
+    using System.Threading.Tasks;
+
+    using Stripe;
+
+    using KeepFitStore.Data;
+    using KeepFitStore.Services.Contracts;
+    
+    public class PaymentService : IPaymentService
+    {
+        private const string DefaultCurrency = "gbp";
+        private const string PaymentDescription = "Paying for supliments in keep fit store";
+        private const string SucceededPayment = "succeeded";
+        private const int DefaultStripeCents = 100;
+
+        private readonly KeepFitDbContext context;
+
+        public PaymentService(KeepFitDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<bool> MakePaymentAsync(string email, string token, decimal amount)
+        {
+            var customersService = new CustomerService();
+            var chargesService = new ChargeService();
+
+            var customer = await customersService.CreateAsync(new CustomerCreateOptions()
+            {
+                Email = email,
+                Source = token
+            });
+
+            var charge = chargesService.Create(new ChargeCreateOptions()
+            {
+                Amount = (long)(amount * DefaultStripeCents),
+                Description = PaymentDescription,
+                Currency = DefaultCurrency,
+                CustomerId = customer.Id,
+                ReceiptEmail = email
+            });
+
+            if (charge.Status == SucceededPayment)
+            {
+                string balanceTransactionId = charge.BalanceTransactionId;
+                return true; 
+            }
+
+            return false; 
+        }
+    }
+}
