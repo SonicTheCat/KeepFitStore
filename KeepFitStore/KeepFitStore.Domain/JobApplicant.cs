48df005241fa460547036b1357103fd1c4a199f1
SonicTheCat <paveliliev90@gmail.com>
2019-08-01 13:44:13 +0300

    Two new entities added - Job Applicant and Job Position

---
+++ KeepFitStore/KeepFitStore.Domain/JobApplicant.cs
@@ -0,0 +1,25 @@
+﻿namespace KeepFitStore.Domain
+{
+    public class JobApplicant
+    {
+        public int Id { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Middlename { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public int Age { get; set; }
+
+        public bool ExperienceInSelling { get; set; }
+
+        public string Bio { get; set; }
+
+        public int PositionId { get; set; }
+
+        public JobPosition Position { get; set; }
+    }
+}
---
+++ KeepFitStore/KeepFitStore.Domain/JobPosition.cs
@@ -0,0 +1,20 @@
+﻿namespace KeepFitStore.Domain
+{
+    using System.Collections.Generic;
+
+    public class JobPosition
+    {
+        public JobPosition()
+        {
+            this.Applicants = new HashSet<JobApplicant>(); 
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public ICollection<JobApplicant> Applicants { get; set; }
+    }
+}
