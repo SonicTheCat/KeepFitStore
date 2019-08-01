d92f762e9f9bef761958cf197cea549e0e7feb30
SonicTheCat <paveliliev90@gmail.com>
2019-08-01 20:43:18 +0300

    new javascript file added for jobs functions

---
+++ KeepFitStore/KeepFitStore.WEB/wwwroot/js/jobs.js
@@ -0,0 +1,29 @@
+﻿function showApplicationDetails() {
+    $(".show-more-details").click(function () {
+
+        let btn = $(this);
+        let detailsDiv = btn.parent().next();
+        let bio = btn.prev();
+
+        if (detailsDiv.attr("opened") === "false") {
+            detailsDiv.attr("opened", "true");
+            btn.text("Hide details");
+            btn.addClass("btn-danger");
+            bio.hide("slow");
+        }
+        else {
+            detailsDiv.attr("opened", "false");
+            btn.text("See more");
+            btn.removeClass("btn-danger");
+            bio.show("slow");
+        }
+
+        detailsDiv.toggle("slow");
+    })
+}
+
+function toggleApplyForJobForm() {
+    $("#show-job-form-btn").click(function () {
+        $("#job-form").toggle("slow");
+    });
+}
