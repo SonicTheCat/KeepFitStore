4d5fe7c7dde8b415af3d3591e6fc991eb0aa1976
SonicTheCat <paveliliev90@gmail.com>
2019-08-01 19:26:47 +0300

    JabApplicant controller added in admin area for listing all send applications

---
+++ KeepFitStore/KeepFitStore.WEB/Areas/Administrator/Controllers/JobApplicantController.cs
@@ -0,0 +1,26 @@
+﻿namespace KeepFitStore.WEB.Areas.Administrator.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using KeepFitStore.Models.ViewModels.JobApplicants;
+    using KeepFitStore.Services.Contracts;
+
+    public class JobApplicantController : AdministratorController
+    {
+        private readonly IJobApplicantService applicantService;
+
+        public JobApplicantController(IJobApplicantService applicantService)
+        {
+            this.applicantService = applicantService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = await this.applicantService.GetAllAsync<IndexJobApplicantViewModel>();
+
+            return this.View(viewModel);
+        }
+    }
+}
