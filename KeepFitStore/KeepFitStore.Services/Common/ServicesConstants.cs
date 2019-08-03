8708c031ec6f09deede67155d55fd10c7ec1ecd9
SonicTheCat <paveliliev90@gmail.com>
2019-08-03 17:40:01 +0300

    ExceptionFilter added!

---
+++ KeepFitStore/KeepFitStore.Services/Common/ServicesConstants.cs
@@ -0,0 +1,41 @@
+﻿namespace KeepFitStore.Services.Common
+{
+    public static class ServicesConstants
+    {
+        public static int InvalidQuantityNumber = 0;
+
+        public const int QuantityDefaultValue = 1;
+
+        public const int InvalidProductsCountInOrder = 0;
+
+        public const int OneRow = 1;
+
+        public const int MinimumBasketValueForNextDayDelivery = 60;
+
+        public const int MinimumBasketValueForStandartDeliery = 20;
+
+        public const int ExpressDeliveryPrice = 15;
+
+        public const int NextDayDeliveryPrice = 10;
+
+        public const int StandartDeliveryPrice = 5;
+
+        public const int ExpressDeliveryHours = 5;
+
+        public const int NextDayDeliveryHours = 24;
+
+        public const int StandartDeliveryHours = 92;
+
+        public const string DescendingOldValue = "Desc";
+
+        public const string DescendingNewValue = " descending";
+
+        public const string PaymentDefaultCurrency = "gbp";
+
+        public const string PaymentDescription = "Paying for supliments in keep fit store";
+
+        public const string SucceededPayment = "succeeded";
+
+        public const int PaymentDefaultStripeCents = 100;
+    }
+}
---
+++ KeepFitStore/KeepFitStore.WEB/Filters/KeepFitExceptionFilter.cs
@@ -0,0 +1,52 @@
+﻿namespace KeepFitStore.WEB.Filters
+{
+    using System.Net;
+    using System.Reflection;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    using KeepFitStore.Services.CustomExceptions;
+    using KeepFitStore.Services.CustomExceptions.Attributes;
+
+    public class KeepFitExceptionFilter : IExceptionFilter
+    {
+        private const string ServerError = "Server error occured";
+
+        private readonly bool isDevelopment;
+
+        public KeepFitExceptionFilter(bool isDevelopment)
+        {
+            this.isDevelopment = isDevelopment;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            HttpStatusCode status = HttpStatusCode.InternalServerError;
+            var message = ServerError;
+
+            var exception = context.Exception;
+            var stackTrace = this.isDevelopment ? exception.StackTrace : string.Empty;
+
+            if (exception is ServiceException)
+            {
+                var exAttr = exception
+                    .GetType()
+                    .GetCustomAttribute(typeof(MyExceptionAttribute))
+                    as MyExceptionAttribute;
+
+                message = exception.Message;
+                status = (HttpStatusCode)exAttr.StatusCode;
+            }
+
+            context.ExceptionHandled = true;
+
+            context.Result = new ObjectResult(new
+            {
+                StatusCode = status,
+                Message = message,
+                StackTrace = stackTrace
+            });
+        }
+    }
+}
