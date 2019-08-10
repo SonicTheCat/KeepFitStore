2c07504249df40b7e456ced9345e449429182c30
SonicTheCat <paveliliev90@gmail.com>
2019-08-10 23:58:22 +0300

    View component added for displaying most ordered products in home page!

---
+++ KeepFitStore/KeepFitStore.WEB/Components/MostOrderedProductsViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using KeepFitStore.Models.ViewModels.Products;
+using KeepFitStore.Services.Contracts;
+using KeepFitStore.WEB.Common;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KeepFitStore.WEB.Components
+{
+    [ViewComponentAttribute(Name = "MostOrdered")]
+    public class MostOrderedProductsViewComponent : ViewComponent
+    {
+        private readonly IProductsService productsService;
+
+        public MostOrderedProductsViewComponent(IProductsService productsService)
+        {
+            this.productsService = productsService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var products = await this.productsService
+                .GetMostOrderedProducts<ProductViewModel>(WebConstants.CountOfProductsToBeShownOnHomePage);
+
+            return this.View(products);
+        }
+    }
+}
